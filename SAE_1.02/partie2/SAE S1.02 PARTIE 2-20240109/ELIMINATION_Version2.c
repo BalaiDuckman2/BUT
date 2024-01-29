8e562452f7e3e1f8445e2b80183eb6c49492fae8
BalaiDuckman2 <raf29450@gmail.com>
2024-01-29 18:12:49 +0100

    fdfd

--- SAE_1.02/partie2/SAE S1.02 PARTIE 2-20240109/ELIMINATION_Version2.c
+++ SAE_1.02/partie2/SAE S1.02 PARTIE 2-20240109/ELIMINATION_Version2.c
@@ -23,7 +23,6 @@
 
    void chargerGrille(tGrille grille, char nomFichier[TAILLE_FICHIER]);
    void affichegrille(tGrille grille);
-   void saisir(int *valeur);
    bool possible(tGrille grille,int ligne,int colonne,int valeur);
    bool grilleComplete(tGrille grille);
    bool backtracking(tGrille grille, int numeroCase);
@@ -39,12 +38,6 @@
    int possibleCaché(tGrille grille, int ligne, int colonne, int valeur);
    void singletonCaché(tGrille tab);
    int nbCaseVide(tGrille tab);
-   int nbCandidatsElimine(tGrille tab);
-   void statistique(tGrille grille, float nbCaseVideInitial,float nbCandidatsInitial,char nomFichier[TAILLE_FICHIER]);
-   void paireNue(tGrille grille);
-   void paireNueCaché(tGrille grille);
-   int cherchecandidat(tGrille grille, int i, int j,int nombre);
-
 
    int main(){
       tGrille grille1;
@@ -53,35 +46,24 @@
       int numLigne, numColonne, valeur,nbCaseInitial,nbCandidatsInitial;
       char nomFic[TAILLE_FICHIER];
       chargerGrille(grille1,nomFic);
-      nbCaseInitial=nbCaseVide(grille1);
-      nbCandidatsInitial=nbCandidatsElimine(grille1);
-      
+      printf("Grille initial\n");
       affichegrille(grille1);
-      
       srand(time(NULL));
-      
       clock_t begin = clock();
       initialise(grille1);
       while(nbCaseVide(grille1)!=0 && nbCase!=nbCase2)
       {
-         
          nbCase=nbCaseVide(grille1);
-         
          singletonNu(grille1);
-         paireNue(grille1)
-         singletonCaché(grille1);  
-                                                           
+         singletonCaché(grille1);                                               
          nbCase2=nbCaseVide(grille1);
-         
-         
       }
-      
-      
       backtracking(grille1,0);
       clock_t end = clock();
+      printf("Grille final\n");
       affichegrille(grille1);
       double  tmpsCPU = (end - begin)*1.0 / CLOCKS_PER_SEC;
-      printf( "\nTemps CPU = %.3f secondes\n",tmpsCPU);
+      printf( "\nTemps CPU = %.6f secondes\n",tmpsCPU);
 
       return EXIT_SUCCESS;
    }
@@ -90,11 +72,9 @@
 
 
    void chargerGrille(tGrille grille, char nomFichier[TAILLE_FICHIER]){
-      
       FILE * f;
-      nomFichier = "MaxiGrilleB.sud";
-      //printf("Nom du fichier : ");
-      //scanf("%s", nomFichier);
+      printf("Nom du fichier : ");
+      scanf("%s", nomFichier);
       f = fopen(nomFichier, "rb");
       if (f==NULL)
       {
@@ -435,276 +415,6 @@ int nbCaseVide(tGrille tab){
    return totale;
    
 }
-int nbCandidatsElimine(tGrille tab){
-   int totale=0;
-   for (int i = 0; i < TAILLE; i++)
-      {
-         for (int j = 0; j < TAILLE; j++)
-         {
-            if (tab[i][j].valeur==0)
-            {
-               
-               for (int x = 1; x < TAILLE+1; x++)
-               {
-                  
-                  if (possible(tab, i, j, x)==true) {
-                     totale++;
-                     
 
-                  }
-               }            
-            }
-            
-            
-            
-         }
-         
-      }
-      return totale;
-}
-void statistique(tGrille grille, float nbCaseVideInitial,float nbCandidatsInitial,char nomFichier[TAILLE_FICHIER]){
-   float taux,pourcentage;
-   float nbCaseVideFinal = nbCaseVide(grille);
-   float nbCandidatsFinal = nbCandidatsElimine(grille);
-   nbCaseVideFinal =nbCaseVideInitial-nbCaseVideFinal;
-   nbCandidatsFinal= nbCandidatsInitial-nbCandidatsFinal;
-
-   if (nbCandidatsFinal==0)
-   {
-      
-      pourcentage=0;
-   }else{
-      pourcentage=(nbCandidatsFinal/nbCandidatsInitial)*100;
-   }
-   if (nbCaseVideFinal==0 )
-   {
-      taux=0;
-   }else{
-      taux=(nbCaseVideFinal/nbCaseVideInitial)*100;
-   }
-   
-   
-   printf("\n\n***** Résultat pour %s ******\n\n",nomFichier);
-   printf("Nombre de cases remplies = %.0f sur %.0f  Taux de remplissage = %.2f%%\n\n",nbCaseVideFinal,nbCaseVideInitial,taux);
-   printf("Nombre de Candidats elimines = %.0f     Pourcentage d'elimination = %.2f%%\n\n",nbCandidatsFinal,pourcentage);
-}
-
-
-void paireNue(tGrille grille){
-   int coord1[2];
-   int coord2[2];
-   bool paire1[2];
-  int x=0;
-  int y=0;
-  bool fin=false;
-   
-   
-   
-   for (int i = 2; i <= 8; i=i+N)
-   {
-      x=i;
-      for (int j = 2; j <= 8; j=j+N)
-      {
-         y=j;
-         printf("oui");
-         
-            while (x!=i-2&&fin==false)
-            {
-               while (y!=j-2 && fin==false)
-               {
-                  if (grille[x][y].valeur==0)
-                  {
-                     
-                     if(grille[x][y].nbCandidats==2)
-                     {
-                        int valpaire=0;
-                        paire1[0]=0;
-                        paire1[1]=0;
-                        coord1[0]=x;
-                        coord1[1]=y;
-                        for (int k = 1; k < 10; k++)
-                        {
-                           if (grille[x][y].candidats[k]==true)
-                           {
-                              paire1[valpaire]=k;
-                              valpaire++;
-                              
-                              fin=true;
-                           }
-                        }
-                     }
-                  }
-                  y--;
-               }
-               
-               x--;
-            }
-            while (x!=i-2&&fin==true)
-            {
-               while (y!=j-2&&fin==true)
-               {
-                  if(grille[x][y].nbCandidats==2)
-                  {
-                     coord2[0]=0;
-                     coord2[1]=0;
-                     if (grille[x][y].candidats[paire1[0]]==true && grille[x][y].candidats[paire1[1]]==true)
-                     {
-                        coord2[0]=x;
-                        coord2[1]=y;
-                        for (int u = i; u > i-2; u--)
-                           {
-                              for (int p = j; p > j-2; p--)
-                              {
-                                 
-                                 if (coord1[0]!=u||coord1[1]!=p)
-                                 {
-                                    if (coord2[0]!=u||coord2[1]!=p)
-                                    {
-                                       
-                                       grille[u][p].candidats[paire1[0]]=false;
-                                       grille[u][p].candidats[paire1[1]]=false;
-                                    }
-                                 }
-                              }
-                           }
-                     }
-                     
-                  }
-                  j--;
-               }
-               x--;
-            }
-            
-         }
-      }
-}
-
-int cherchecandidat(tGrille grille, int i, int j,int nombre){
-   int compteur=0;
-   for (int x = i; x > i-2; x--)
-   {
-      for (int y = j; y > j-2; y--)
-      {
-         if(grille[x][y].valeur==0){
-            
-               if (grille[x][y].candidats[nombre]==true)
-               {
-                  compteur++;
-                  
-                  
-               }
-               
-            
-            
-         }
-      }
-   }
-   return compteur;
-}
-   
-void paireNueCaché(tGrille grille){
-   int compteur=0;
-   bool nbCandidats[10];
-   
-   
-   for (int i = 2; i <= 8; i=i+N)
-   {
-      for (int j = 2; j <= 8; j=j+N)
-      {
-         for (int k = 1; k < 10; k++)
-         {
-            
-            nbCandidats[k]=false;
-         }
-         compteur=0;
-         for (int k = 1; k < 10; k++)
-         {
-            
-            if(cherchecandidat(grille,i,j,k)==2){
-               nbCandidats[k]=true;
-               compteur++;
-            }
-            
-         }
-         if(compteur>2)
-         {
-            for (int k = 1; k < 10; k++)
-            {
-               if (nbCandidats[k]==true)
-               {
-                  for (int p = 1; p < 10; p++)
-                  {
-                     if (nbCandidats[p]==true&&p!=k)
-                     {
-                        for (int x = i; x > i-2; x--)
-                        {
-                           for (int y = j; y > j-2; y--)
-                           {
-                              compteur=0;
-                              
-                              if (grille[x][y].candidats[k]==true&&grille[x][y].candidats[p]==true)
-                              {
-                                    compteur++;
-                                    
-                                    
-                              }
-
-                                 
-                              
-                              if (compteur==1)
-                              {
-                                 
-                                 for (int m = x; m > i-2; m--)
-                                 {
-                                    for (int l = y; l > j-2; l--)
-                                    {
-                                       compteur==0;
-                                       if (m!=x||l!=y)
-                                       {
-                                          
-                                          if (grille[m][l].candidats[k]==true&&grille[m][l].candidats[p]==true)
-                                          {
-                                             compteur++;
-                                          }
-
-                                             
-                                          if (compteur==1)
-                                          {
-                                             for (int b = 1; b < 10; b++)
-                                             {
-                                                if (b!=k&&b!=p)
-                                                {
-                                                   grille[x][y].candidats[b]=false;
-                                                   grille[m][l].candidats[b]=false;
-                                                }
-                                                
-                                             }
-                                             
-                                             
-                                          }
-                                          
-                                       }
-                                    }
-                              }
-                              
-                              
-                           }
-                     
-                        }
-                  
-                  }
-               
-         }
-                  
-      }
-
-         
-   }
-
-}
-}
-}  
-} 
-}
 
 
